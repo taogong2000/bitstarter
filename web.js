e96562275364c0a33331bc106a4beab30a526bce
Ubuntu <ubuntu@ip-172-31-42-55.us-west-2.compute.internal>
2014-08-26 21:02:50 +0000

    fix typo in web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createSever(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     response.send('Hello World2!');
@@ -10,5 +10,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
-
-
